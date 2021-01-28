b79de713e481f628fae64ae2679e010b8e0c8efc
gildydtjd <gildydtjd@naver.com>
2021-01-29 07:18:12 +0900

    mouse animation

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -9,19 +9,11 @@ function Header(props) {
         var navHeight = $(".header").height(); 
     
         $(".header").show();
-        //스크롤시 나타날 객체 미리 숨기기
     
-        $(window).scroll(function(){  // 윈도우 스크롤 기능 작동
+        $(window).scroll(function(){
             var rollIt = $(this).scrollTop() >= navHeight; 
-    // 스크롤의 정도가 navHeight 의 값을 넘었을 때 == 윈도우 스크롤의 값이 navHeight 의 높이와 같거나 크다
-    
-    /*
-    scrollTop 은 윈도우에서 스크롤의 위치가 가장 상위에 있다는 의미로 해석
-    스크롤의 위치가 화면 아래일수록 == scrollTop 의 값이 커짐
-    */
     
         if(rollIt){ 
-            //윈도우 스크롤 기능의 값이 navHeight 의 높이와 같거나 크면
                 $(".header").show().css({"backgroundColor":"black"});
             }
             else{
