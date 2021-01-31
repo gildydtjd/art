9c2fbdce08c1614ab6fcb07ef8ed4343d54af7b2
gildydtjd <gildydtjd@naver.com>
2021-02-01 08:08:03 +0900

    css

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -6,11 +6,11 @@ import MainContent from '../Contents/MainContent.js';
 import React from 'react';
 
 function Home(props) {
-    // const style = {
-    // backgroundImage: `url(${process.env.PUBLIC_URL}/assets/img/art7.jpg)`
-    // }
+    const style = {
+    backgroundImage: `url(${process.env.PUBLIC_URL}/assets/img/art7.jpg)`
+    }
     return (
-        <div className="home" >
+        <div className="home" style={style}>
                 <Header />
                 <HeaderContainer />
                 <MainContent />
